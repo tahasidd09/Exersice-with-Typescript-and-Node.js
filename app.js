bc78bdfb485e0bbe42d412872d48cea05170b24b
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-02 17:21:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-// Define an array to store a list of famous cities
-var cities = [
-    "Delhi",
-    "Bangkok",
-    "Beijing",
-    "jakarta",
-    "Cairo",
-    "Taipei",
-    "Moscow",
-    "Cape Town",
-    "Beijing",
-    "London"
+// Create an array of car objects
+var cars = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        year: 2020,
+        color: "Blue"
+    },
+    {
+        make: "Honda",
+        model: "Civic",
+        year: 2018,
+        color: "Red"
+    },
+    {
+        make: "Ford",
+        model: "Mustang",
+        year: 2021,
+        color: "Black"
+    },
+    {
+        make: "Chevrolet",
+        model: "Impala",
+        year: 2019,
+        color: "White"
+    },
+    {
+        make: "Tesla",
+        model: "Model 3",
+        year: 2022,
+        color: "Silver"
+    }
 ];
-// Function to display the list of cities
-function displayCities(cityList) {
-    console.log("List of Famous Cities:");
-    cityList.forEach(function (city, index) {
-        console.log("".concat(index + 1, ". ").concat(city));
+// Function to display information about each car
+function displayCars(carList) {
+    console.log("List of Cars:");
+    carList.forEach(function (car, index) {
+        console.log("".concat(index + 1, ". ").concat(car.year, " ").concat(car.make, " ").concat(car.model, " (").concat(car.color, ")"));
     });
 }
-// Display the list of cities
-displayCities(cities);
+// Display the list of cars
+displayCars(cars);
--- app.ts
+++ app.ts
@@ -1,24 +1,52 @@
-// Define an array to store a list of famous cities
-const cities: string[] = [
-    "Delhi",
-    "Bangkok",
-    "Beijing",
-    "jakarta",
-    "Cairo",
-    "Taipei",
-    "Moscow",
-    "Cape Town",
-    "Beijing",
-    "London"
+// Define a type for a car
+type Car = {
+    make: string;
+    model: string;
+    year: number;
+    color: string;
+};
+
+// Create an array of car objects
+const cars: Car[] = [
+    {
+        make: "Toyota",
+        model: "Camry",
+        year: 2020,
+        color: "Blue"
+    },
+    {
+        make: "Honda",
+        model: "Civic",
+        year: 2018,
+        color: "Red"
+    },
+    {
+        make: "Ford",
+        model: "Mustang",
+        year: 2021,
+        color: "Black"
+    },
+    {
+        make: "Chevrolet",
+        model: "Impala",
+        year: 2019,
+        color: "White"
+    },
+    {
+        make: "Tesla",
+        model: "Model 3",
+        year: 2022,
+        color: "Silver"
+    }
 ];
 
-// Function to display the list of cities
-function displayCities(cityList: string[]): void {
-    console.log("List of Famous Cities:");
-    cityList.forEach((city, index) => {
-        console.log(`${index + 1}. ${city}`);
+// Function to display information about each car
+function displayCars(carList: Car[]): void {
+    console.log("List of Cars:");
+    carList.forEach((car, index) => {
+        console.log(`${index + 1}. ${car.year} ${car.make} ${car.model} (${car.color})`);
     });
 }
 
-// Display the list of cities
-displayCities(cities);
+// Display the list of cars
+displayCars(cars);
