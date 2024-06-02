e85aba9565e3d2a33d13c51afc66cc2ca23b344c
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-02 18:03:27 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-let usernames: string[] = ['admin', 'Eric', 'John', 'Alice', 'Emily'];
+let usernames: string[] = []; // Empty array
 
-for (let username of usernames) {
-    if (username === 'admin') {
-        console.log("Hello admin, would you like to see a status report?");
-    } else {
-        console.log(`Hello ${username}, thank you for logging in again.`);
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    for (let username of usernames) {
+        if (username === 'admin') {
+            console.log("Hello admin, would you like to see a status report?");
+        } else {
+            console.log(`Hello ${username}, thank you for logging in again.`);
+        }
     }
 }
