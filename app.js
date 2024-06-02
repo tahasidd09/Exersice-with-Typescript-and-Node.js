57108d85fdc539e3737aa640eff6bb386cadd7f8
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-02 17:20:31 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var guestList = ['Taha', "Anas", "Shahroz ", "Ali"];
-var dontcome = guestList[0];
-console.log(dontcome, "He will not come");
-guestList.splice(0, 1, "Zain");
-console.log("Good news ! we have found a bigger table for dinner");
-guestList.unshift("Taha");
-guestList.push("babar");
-var middle = Math.floor(guestList.length / 2);
-guestList.splice(middle, 0, "Amir");
-console.log('updated list of our guest');
-guestList.forEach(function (element) { console.log("HI ".concat(element, ",would you like to dinner with me?")); });
-console.log("unfortunately, the new dinner table wont arrive on time, so i can only invite two guests to dinner with me");
-while (guestList.length > 2) {
-    var kicked = guestList.pop();
-    console.log("sorry ".concat(kicked, " you are not invited to dinner"));
+// Define an array to store a list of famous cities
+var cities = [
+    "Delhi",
+    "Bangkok",
+    "Beijing",
+    "jakarta",
+    "Cairo",
+    "Taipei",
+    "Moscow",
+    "Cape Town",
+    "Beijing",
+    "London"
+];
+// Function to display the list of cities
+function displayCities(cityList) {
+    console.log("List of Famous Cities:");
+    cityList.forEach(function (city, index) {
+        console.log("".concat(index + 1, ". ").concat(city));
+    });
 }
-guestList.forEach(function (element) { console.log("HI ".concat(element, ", would you like to dinner with me?")); });
-guestList.pop();
-guestList.pop();
-console.log("Empty List", guestList);
+// Display the list of cities
+displayCities(cities);
--- app.ts
+++ app.ts
@@ -1,38 +1,24 @@
-let guestList: string[] = ['Taha', "Anas" , "Shahroz ", "Ali"]
+// Define an array to store a list of famous cities
+const cities: string[] = [
+    "Delhi",
+    "Bangkok",
+    "Beijing",
+    "jakarta",
+    "Cairo",
+    "Taipei",
+    "Moscow",
+    "Cape Town",
+    "Beijing",
+    "London"
+];
 
-  let dontcome = guestList[0]
-   
-  console.log (dontcome, "He will not come")
-
-  guestList.splice(0, 1, "Zain");
- 
-  console.log("Good news ! we have found a bigger table for dinner");
-
-  guestList.unshift("Taha");
-
-  guestList.push("babar")
- 
-  let middle : number= Math.floor(guestList.length / 2)
-
-  guestList.splice(middle,0,"Amir");
-   
-console.log('updated list of our guest')
- guestList.forEach(element =>{console.log(`HI ${element},would you like to dinner with me?`)});
-
-console.log("unfortunately, the new dinner table wont arrive on time, so i can only invite two guests to dinner with me")
-
-while (guestList.length > 2) {
-  let kicked = guestList.pop();
-  console.log(`sorry ${kicked} you are not invited to dinner`)
+// Function to display the list of cities
+function displayCities(cityList: string[]): void {
+    console.log("List of Famous Cities:");
+    cityList.forEach((city, index) => {
+        console.log(`${index + 1}. ${city}`);
+    });
 }
-guestList.forEach(element =>{console.log(`HI ${element}, would you like to dinner with me?`)});
 
- guestList.pop()
- guestList.pop()    
-
- console.log("Empty List",guestList)
- 
-
-//  Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-
-
+// Display the list of cities
+displayCities(cities);
