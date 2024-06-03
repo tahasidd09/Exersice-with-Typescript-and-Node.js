3cb21d248a84464db18fec23df9efeea26f05556
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-03 08:19:41 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+function make_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function show_magicians(magicians) {
-    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
-        var magician = magicians_1[_i];
-        console.log(magician);
+    console.log("Making a sandwich with the following items:");
+    for (var _a = 0, items_1 = items; _a < items_1.length; _a++) {
+        var item = items_1[_a];
+        console.log("- ".concat(item));
     }
+    console.log("Enjoy your sandwich!\n");
 }
-function make_great(magicians) {
-    var great_magicians = [];
-    for (var _i = 0, magicians_2 = magicians; _i < magicians_2.length; _i++) {
-        var magician = magicians_2[_i];
-        great_magicians.push("the Great ".concat(magician));
-    }
-    return great_magicians;
-}
-// Create an array of magician's names
-var magicians = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller', 'Criss Angel'];
-// Create a new array with the modified magician names
-var great_magicians = make_great(__spreadArray([], magicians, true));
-// Show the original magicians
-console.log("Original Magicians:");
-show_magicians(magicians);
-// Show the great magicians
-console.log("\nGreat Magicians:");
-show_magicians(great_magicians);
+// Call the function with different numbers of arguments
+make_sandwich('ham', 'cheese', 'lettuce');
+make_sandwich('turkey', 'bacon', 'avocado', 'tomato');
+make_sandwich('peanut butter', 'jelly');
--- app.ts
+++ app.ts
@@ -1,27 +1,13 @@
-function show_magicians(magicians: string[]): void {
-    for (let magician of magicians) {
-        console.log(magician);
+function make_sandwich(...items: string[]): void {
+    console.log("Making a sandwich with the following items:");
+    for (let item of items) {
+        console.log(`- ${item}`);
     }
+    console.log("Enjoy your sandwich!\n");
 }
 
-function make_great(magicians: string[]): string[] {
-    let great_magicians: string[] = [];
-    for (let magician of magicians) {
-        great_magicians.push(`the Great ${magician}`);
-    }
-    return great_magicians;
-}
+// Call the function with different numbers of arguments
+make_sandwich('ham', 'cheese', 'lettuce');
+make_sandwich('turkey', 'bacon', 'avocado', 'tomato');
+make_sandwich('peanut butter', 'jelly');
 
-// Create an array of magician's names
-let magicians: string[] = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller', 'Criss Angel'];
-
-// Create a new array with the modified magician names
-let great_magicians = make_great([...magicians]);
-
-// Show the original magicians
-console.log("Original Magicians:");
-show_magicians(magicians);
-
-// Show the great magicians
-console.log("\nGreat Magicians:");
-show_magicians(great_magicians);
