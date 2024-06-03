f09e84d4705a7db8deb07e9b7751472e5e2fd48c
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-03 08:17:26 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-function make_album(artist, title, tracks) {
-    var album = { artist: artist, title: title };
-    if (tracks !== undefined) {
-        album.tracks = tracks;
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
     }
-    return album;
 }
-// Create three albums and print the return values
-var album1 = make_album('Taylor Swift', 'Evermore');
-var album2 = make_album('Ed Sheeran', 'Divide');
-var album3 = make_album('Adele', '30', 12);
-console.log(album1);
-console.log(album2);
-console.log(album3);
-// Create an album with the number of tracks
-var album4 = make_album('BTS', 'Map of the Soul: 7', 20);
-console.log(album4);
+// Create an array of magician's names
+var magicians = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller', 'Criss Angel'];
+// Call the function and pass the array of magician's names
+show_magicians(magicians);
--- app.ts
+++ app.ts
@@ -1,26 +1,11 @@
-interface Album {
-    artist: string;
-    title: string;
-    tracks?: number;
-}
-
-function make_album(artist: string, title: string, tracks?: number): Album {
-    let album: Album = { artist, title };
-    if (tracks !== undefined) {
-        album.tracks = tracks;
+function show_magicians(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
     }
-    return album;
 }
 
-// Create three albums and print the return values
-let album1 = make_album('Taylor Swift', 'Evermore');
-let album2 = make_album('Ed Sheeran', 'Divide');
-let album3 = make_album('Adele', '30', 12);
+// Create an array of magician's names
+let magicians: string[] = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller', 'Criss Angel'];
 
-console.log(album1);
-console.log(album2);
-console.log(album3);
-
-// Create an album with the number of tracks
-let album4 = make_album('BTS', 'Map of the Soul: 7', 20);
-console.log(album4);
+// Call the function and pass the array of magician's names
+show_magicians(magicians);
