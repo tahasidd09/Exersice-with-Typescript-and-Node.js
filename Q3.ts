744f4cdda8a93c9bee9e03b3c35db97b0338d25c
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-03 09:49:26 +0500

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,10 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let firstName: string = "Taha Siddiqui"
+
+// console.log(firstName.toLocaleLowerCase());
+// console.log(firstName.toLocaleUpperCase());
+// console.log(firstName.charAt(0).toUpperCase() +firstName.slice(1));
+
+
+
