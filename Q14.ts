b2d85ce767952d881fc249bad33317ef459f9498
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-05 20:44:37 +0500

    Add files via upload

---
+++ Q14.ts
@@ -0,0 +1,5 @@
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+let guestList = ["Taha" , "Ibrahim" , "Ibrar" , "Zaibi"];
+
+guestList.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me?`));
