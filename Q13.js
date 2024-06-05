467142680714a57c4469e66766a23ee1d0ac4613
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-05 20:43:50 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,3 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var transportationModes = ["Sport Bike", "Bus", "Car", "Vehicle"];
+transportationModes.forEach(function (mode) { return console.log("\u201CI would like to own a ".concat(mode)); });
