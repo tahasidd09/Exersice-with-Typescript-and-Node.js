b0c270a11bfe9635069a90f13d819554db045150
tahasidd09 <161510569+tahasidd09@users.noreply.github.com>
2024-06-05 20:41:34 +0500

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,37 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// // Addition 
+// console.log(4 + 4);
+//  Substraction
+// cocclsnsole.log(12 - 4);
+// // Multiplication
+// console.log(2 * 4);
+// // Division
+// console.log(16 / 2);
+
+// const pencils = 21
+// const students = 5 
+
+// const mod = pencils % students
+
+// console.log(mod)
+
+// const num1 = 5
+// const num2 = 2
+
+// const power = num1 ** num2
+// console.log(power)
+
+// const firstName = "Taha"
+// const lastName = "9"
+
+// console.log(firstName + " " + lastName)
+
+
+console.log (10 == 10);
+console.log (10 === 10);
+console.log (10 !=5);
+console.log (10 > 5);
+console.log (10 < 5);
+
+
